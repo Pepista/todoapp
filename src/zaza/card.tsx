175407b58009ac0ffe2e40467a56fd476eaa2c88
2022CechakPetr <pcechak07@gmail.com>
2024-04-18 12:57:36 +0200

    Papagaja

---
+++ src/zaza/card.tsx
@@ -0,0 +1,11 @@
+export default function Card()
+{
+    return(
+      <main className="bg-black-100">
+          <div className="text-1xl text-red-800">
+                <h1>asdasdssdsa</h1>
+          </div>
+
+      </main>
+    );
+}
